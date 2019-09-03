e200da7406a49a70290b4ceb98092df9f9fd3bf9
gionellef <gionellef@gmail.com>
2019-09-03 10:31:32 +0800

    Updates

---
+++ precache-manifest.2feaa8a2bec4e9eabaad9607fb50c5e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6852baeb1c7ab814daf84432eaa55f3c",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "10d84f2b7b184372cec1",
+    "url": "/react-tetris/static/css/main.6b7ee09b.chunk.css"
+  },
+  {
+    "revision": "acd81646e67dd47cc3d5",
+    "url": "/react-tetris/static/js/2.60de6f26.chunk.js"
+  },
+  {
+    "revision": "10d84f2b7b184372cec1",
+    "url": "/react-tetris/static/js/main.e74f9d64.chunk.js"
+  },
+  {
+    "revision": "569332aecf4ad6640d6a",
+    "url": "/react-tetris/static/js/runtime~main.43a5b001.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
